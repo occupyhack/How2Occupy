08f418fa34a6c8d23fe31363dbe51cb33719c1ad
Joel Moore <joelbm24@gmail.commit>
2011-10-15 16:09:58 -0700

    work!

--- how2occupy/views.py
+++ how2occupy/views.py
@@ -1,9 +1,9 @@
 from jsonrpc import jsonrpc_method
 
-@jsonrpc_method('myapp.sayHello')
+@jsonrpc_method('how2occupy.sayHello')
 def whats_the_time(request, name='Lester'):
   return "Hello %s" % name
 
-@jsonrpc_method('myapp.gimmeThat', authenticated=True)
+@jsonrpc_method('how2occupy.gimmeThat', authenticated=True)
 def something_special(request, secret_data):
   return {'sauce': ['authenticated', 'sauce']}
