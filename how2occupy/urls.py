fe53e7fef1397b94331fad92602389578a30adf3
Joel Moore <joelbm24@gmail.commit>
2011-10-15 13:51:07 -0700

    api not working still

--- how2occupy/urls.py
+++ how2occupy/urls.py
@@ -2,10 +2,8 @@
 main url configuration file for the askbot site
 """
 from django.conf.urls.defaults import patterns, include, handler404, handler500, url
-from djangorestframework.resources import ModelResource
 from djangorestframework.views import ListOrCreateModelView, InstanceModelView
-from djangorestframework.views import ListOrCreateModelView, InstanceModelView
-from how2occupy.mobile_resources import MyModelResource
+from modelresourceexample.resources import MyModelResource
 
 from django.conf import settings
 
@@ -20,7 +18,7 @@ urlpatterns = patterns('',
     (r'^followit/', include('followit.urls')),
     (r'^robots.txt$', include('robots.urls')),
     url(r'^$', ListOrCreateModelView.as_view(resource=MyModelResource), name='mobile_resource'),
-    url(r'^api/$', InstanceModelView.as_view(resource=MyModelResource)),
+    url(r'^(?P<pk>[0-9]+)/$', InstanceModelView.as_view(resource=MyModelResource)),
 )
 
 if 'rosetta' in settings.INSTALLED_APPS:
