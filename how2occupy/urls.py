9a2263d9f89834b69bfb326af61301ebd6e54aba
Joel Moore <joelbm24@gmail.commit>
2011-10-15 13:09:41 -0700

    BROKEN: tried to add rest

--- how2occupy/urls.py
+++ how2occupy/urls.py
@@ -2,6 +2,10 @@
 main url configuration file for the askbot site
 """
 from django.conf.urls.defaults import patterns, include, handler404, handler500, url
+from djangorestframework.resources import ModelResource
+from djangorestframework.views import ListOrCreateModelView, InstanceModelView
+from djangorestframework.views import ListOrCreateModelView, InstanceModelView
+from modelresourceexample.resources import MyModelResource
 from django.conf import settings
 
 from django.contrib import admin
@@ -14,6 +18,8 @@ urlpatterns = patterns('',
     (r'^settings/', include('askbot.deps.livesettings.urls')),
     (r'^followit/', include('followit.urls')),
     (r'^robots.txt$', include('robots.urls')),
+    url(r'^$', ListOrCreateModelView.as_view(resource=MyModelResource), name='mobile_resource'),
+    url(r'^(?P<pk>[0-9]+)/$', InstanceModelView.as_view(resource=MyModelResource)),
 )
 
 if 'rosetta' in settings.INSTALLED_APPS:
