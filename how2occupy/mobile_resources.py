98261d85168902c37931e7834761941f20709938
Joel Moore <joelbm24@gmail.commit>
2011-10-15 16:04:05 -0700

    blah

--- how2occupy/mobile_resources.py
+++
@@ -1,7 +0,0 @@
-from djangorestframework.resources import ModelResource
-from how2occupy.mobile_models import MyModel
-
-class MyModelResource(ModelResource):
-    model = MyModel
-    fields = ('foo', 'bar', 'baz', 'url')
-    ordering = ('created',)
