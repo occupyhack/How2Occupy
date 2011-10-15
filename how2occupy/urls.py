b15e6f2bf472e048e03490bb18d80f98c0b6cc9a
Joel Moore <joelbm24@gmail.commit>
2011-10-15 13:52:32 -0700

    api not working still

--- how2occupy/urls.py
+++ how2occupy/urls.py
@@ -3,7 +3,7 @@ main url configuration file for the askbot site
 """
 from django.conf.urls.defaults import patterns, include, handler404, handler500, url
 from djangorestframework.views import ListOrCreateModelView, InstanceModelView
-from modelresourceexample.resources import MyModelResource
+from how2occupy.resources import MyModelResource
 
 from django.conf import settings
 
