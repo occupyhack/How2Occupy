49af6c5e877ae0ffe7b7ecc03878ff9256c4d0f1
Joel Moore <joelbm24@gmail.commit>
2011-10-15 16:32:59 -0700

    blah

--- how2occupy/views.py
+++ how2occupy/views.py
@@ -1,9 +1,9 @@
-from jsonrpc import jsonrpc_method
+#from jsonrpc import jsonrpc_method
 
-@jsonrpc_method('how2occupy.sayHello')
-def whats_the_time(request, name='Lester'):
-  return "Hello %s" % name
+#@jsonrpc_method('how2occupy.sayHello')
+#def whats_the_time(request, name='Lester'):
+  #return "Hello %s" % name
 
-@jsonrpc_method('how2occupy.gimmeThat', authenticated=True)
-def something_special(request, secret_data):
-  return {'sauce': ['authenticated', 'sauce']}
+#@jsonrpc_method('how2occupy.gimmeThat', authenticated=True)
+#def something_special(request, secret_data):
+  #return {'sauce': ['authenticated', 'sauce']}
